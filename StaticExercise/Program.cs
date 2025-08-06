08f1784d4bbaf9738c67a0b4f12899ac3264274a
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-08-05 22:56:50 -0400

    first ex done

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace StaticExercise
+﻿using System;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celsius {celsius} ");
+            Console.WriteLine($"fahrenheit {fahrenheit} ");
         }
     }
 }
+//public static class TempConverter
+    //{
+       // public static double FahrenheitToCelsius(double fahrenheit)
+        //{
+          // return (fahrenheit - 32) / 1.8;
+        //}
+
+        //public static double CelsiusToFahrenheit(double celsius)
+       //{
+        //   return (celsius * 9) / 5 + 32;
+       //}
+  // }
+//}
+
+        
+
+
